1fecfa4b64730df927417f56caf7bdddf7c604ab
cabbe <perle@CABBESDEMON>
2024-05-20 18:21:51 +0200

    test

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,26 @@
 import pytest
+from unittest.mock import MagicMock
+from src.controllers.recipecontroller import RecipeController
 
 # add your test case implementation here
 @pytest.mark.unit
 def test():
     pass
+
+@pytest.mark.unit
+def id_1_Success_Optimal():
+
+    readinessDict = {}
+    readinessDict["bananaBread"] = 0.1
+    readinessDict["cupcakes"] = 0.0
+
+    mockedRC = MagicMock()
+    mockedRC.get_readiness_of_recipes.return_value = readinessDict
+    rc = RecipeController(mockedRC)
+
+    assert rc.get_recipe() 
+
+@pytest.mark.unit
+def id_1_Success_Random():
+
+    assert True == True
